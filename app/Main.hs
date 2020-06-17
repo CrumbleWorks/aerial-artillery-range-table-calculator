635345cd289fba34afc6da3199b102dc17b651af
McDonnough <psbaechli@gmail.com>
2020-06-17 20:51:50 +0200

    remove unnecessary code

--- app/Main.hs
+++ app/Main.hs
@@ -7,22 +7,10 @@ instance T.Tabulate RangeInfo T.DoNotExpandWhenNested
 
 main :: IO ()
 main = do
-    --print "Luftschiff, yo."
-
     let velocity = 800
     let angle = 20
-    --let measurements = [30,31..60]
     let measurements = [5,10..60]
 
     let rangeInfos = map (rangeInfo angle velocity) measurements
-
-    {-let d1 = distance 20 800 60
-    putStrLn $ (show d1) ++ "m"
-
-    let t1 = airtime 20 800 19587.94
-    putStrLn $ (show t1) ++ "s"
-
-    let r1 = rangeInfo 20 800 60
-    putStrLn $ show r1-}
 
     T.printTable rangeInfos
