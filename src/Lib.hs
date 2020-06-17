84a6784af6b15424d78b0b4ff394ea293ac5f63a
McDonnough <psbaechli@gmail.com>
2020-06-17 19:45:45 +0200

    move calculations to separate module

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,6 +1,43 @@
+{-# LANGUAGE DeriveDataTypeable    #-}
+{-# LANGUAGE DeriveGeneric         #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+
 module Lib
-    ( someFunc
+    ( RangeInfo(..)
+    , rangeInfo
     ) where
 
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+import           Data.Data
+import qualified GHC.Generics              as G
+import qualified Text.PrettyPrint.Tabulate as T
+
+data RangeInfo = RangeInfo
+    { time_     :: Integer
+    , angle_    :: Integer
+    , velocity_ :: Float
+    , range_    :: Float
+    } deriving (Data, G.Generic, Show)
+
+-- | Converts from degrees to radians.
+radians :: Float -> Float
+radians degrees = degrees * (pi / 180)
+
+-- | Calculates the cosine of an angle in degrees.
+cos' :: Float -> Float
+cos' degrees = cos $ radians degrees
+
+timeOfFlight :: Integer -> Float -> Float -> Float
+timeOfFlight angle velocity range = range / (velocity * cos (fromIntegral angle))
+
+distance :: Integer -> Float -> Integer -> Float
+distance angle velocity time = velocity * fromInteger time * cos' (fromIntegral angle)
+
+rangeInfo :: Integer -> Float -> Integer -> RangeInfo
+rangeInfo angle velocity time = do
+    let shellRange = distance angle velocity time
+    RangeInfo {
+        time_ = time,
+        angle_ = angle,
+        velocity_ = velocity,
+        range_ = shellRange
+    }
